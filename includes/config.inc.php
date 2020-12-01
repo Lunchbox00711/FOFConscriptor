fbf4d1a9fa7b08d198b8056de9f125eb1a0f2af7
Jon Baker <jbaker@wgm.us>
2020-12-01 12:03:23 -0600

    Fixing a few other issues with constants

--- includes/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = "root";
-$password = "";
-$database = "conscriptor";
-$host = "localhost";
-$league_page = "https://myfootballnow.com";
-$mysql = mysqli_connect($host, $user, $password) or die ("I cannot connect to the database server because: ".mysqli_error($mysql));
-mysqli_select_db($mysql, $database) or die ("I cannot connect to the database because: ".mysqli_error($mysql));
-define ('kAdminUser', '0');
